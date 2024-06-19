8915bdac68347c884c7c10cebbb617eaaaabc50e
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-19 21:46:32 +0545

    03 even odd complete

--- _03_even_odd.py
+++ _03_even_odd.py
@@ -7,8 +7,19 @@ def even():
         print(i,end=" ")
     
 def odd():
-    print("Odd numbers from 30 to 50 are")
+    print("\n\nOdd numbers from 30 to 50 are")
     for i in range(31,50,2):
         print(i,end=" ")
         
+def main():
+    even_thread = threading.Thread(target=even)
+    odd_thread = threading.Thread(target=odd)
     
+    even_thread.start()
+    odd_thread.start()
+    
+    even_thread.join()
+    odd_thread.join()
+
+if __name__ == "__main__":
+    main()
