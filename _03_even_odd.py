6279432ef47d30ac1d93f26c56d12652d837e728
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-19 21:34:05 +0545

    even and odd function

--- _03_even_odd.py
+++ _03_even_odd.py
@@ -1,1 +1,14 @@
 # Write a python program to create two threads to find even and odd from 30 to 50
+
+import threading
+def even():
+    print("Even numbers from 30 to 50 are")
+    for i in range(30,51,2):
+        print(i,end=" ")
+    
+def odd():
+    print("Odd numbers from 30 to 50 are")
+    for i in range(31,50,2):
+        print(i,end=" ")
+        
+    
