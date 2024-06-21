cee5c6bbf39e82c2e0f1204a0b6efb818811b5c6
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-21 20:54:39 +0545

    merge sort function

--- _05_merge_sort.py
+++ _05_merge_sort.py
@@ -1,2 +1,18 @@
 # Write a python program to implement multi-threaded merge sort algorithm
 import threading
+
+def merge_sort(arr) ->list:
+    if len(arr) <=1 :
+        return arr
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left,right)
+    
+def merge(left,right) ->list:
+    merged_list = []
+    
+    return merged_list
