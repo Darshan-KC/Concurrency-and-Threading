b12362eed969e8c606b5130740e6c7ccf7d35c99
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-27 21:40:44 +0545

    partition function

--- _06_quick_sort.py
+++ _06_quick_sort.py
@@ -1,3 +1,15 @@
 # Write a Python program to implement a multi-threaded quicksort algorithm.
 
 import threading
+
+def partition(nums, low, high):
+    i = low - 1
+    pivot = nums[high]
+  
+    for j in range(low, high):
+        if nums[j] <= pivot:
+            i = i + 1
+            nums[i], nums[j] = nums[j], nums[i]
+  
+    nums[i + 1], nums[high] = nums[high], nums[i + 1]
+    return i + 1
