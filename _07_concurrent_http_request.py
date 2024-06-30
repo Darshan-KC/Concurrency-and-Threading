6084f3b8d4deff708b0458cafbbe539540741fef
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-30 21:14:31 +0545

    07 concurrent http request complete

--- _07_concurrent_http_request.py
+++ _07_concurrent_http_request.py
@@ -3,11 +3,30 @@
 import requests
 import threading
 
-def http_request():
-    pass 
+def http_request(url):
+    response = requests.get(url)
+    print(f"Response from {url} : {response.status_code}")
 
 def main() ->None:
-    pass
+    # List of URLs to make requests to
+    urls = [
+        "https://www.example.com",
+        "https://www.google.com",
+        "https://www.wikipedia.org",
+        "https://www.python.org"
+    ]
+    
+    # create a threads 
+    threads = []
+    
+    for url in urls:
+        thread = threading.Thread(target=http_request,args=(url,))
+        thread.start()
+        threads.append(thread)
+        
+    # Wait for all the threads
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
     main()
